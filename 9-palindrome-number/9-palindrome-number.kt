1cef03da0340a570d74333bdb7aebd408e2d5d36
Eunjin Kim <57036748+ejkim-dev@users.noreply.github.com>
2022-07-08 22:52:57 +0900

    Time: 502 ms (22.39%), Space: 48.6 MB (5.46%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.kt
+++ 9-palindrome-number/9-palindrome-number.kt
@@ -1,5 +1,19 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        return x.toString() == x.toString().reversed()
+        return when {
+            x == 0 -> true
+            x < 0 -> false
+            else -> {
+                var temp = x
+                var strReverse: String = ""
+                var i = 1
+                while (temp > 0) {
+                    strReverse += temp % 10
+                    temp /= 10
+                    i++
+                }
+                x.toString() == strReverse
+            }
+        }
     }
 }

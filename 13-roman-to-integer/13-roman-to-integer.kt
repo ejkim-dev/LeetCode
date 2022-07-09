b2a652308d3134b184cb991e12c51f3dcdbe9207
Eunjin Kim <57036748+ejkim-dev@users.noreply.github.com>
2022-07-09 23:50:24 +0900

    Time: 509 ms (15.11%), Space: 43.8 MB (25.44%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.kt
+++ 13-roman-to-integer/13-roman-to-integer.kt
@@ -2,12 +2,12 @@ class Solution {
     fun romanToInt(s: String): Int {
         val romanMap = mapOf<Char, Int>('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500, 'M' to 1000)
         var result = 0
-        for (i in 1 .. s.length) {
-            when {
-                i == s.length -> result += romanMap.getValue(s[i-1])
-                romanMap.getValue(s[i-1]) < romanMap.getValue(s[i]) -> result -= romanMap.getValue(s[i-1])
-                else -> result += romanMap.getValue(s[i-1])
-            }
+        var beforeValue = 1000
+        s.forEach {
+            val value = romanMap.getOrDefault(it, 0)
+            if (value > beforeValue) result -= beforeValue * 2
+            result += value
+            beforeValue = value
         }
         return result
     }

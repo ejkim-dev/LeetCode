53583648a5669b171fdeb6009f8254e609b5237e
Eunjin Kim <57036748+ejkim-dev@users.noreply.github.com>
2022-07-13 22:12:46 +0900

    Time: 233 ms (54.58%), Space: 36.3 MB (14.26%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.kt
+++ 20-valid-parentheses/20-valid-parentheses.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun isValid(s: String): Boolean {
+        val map = mapOf(')' to '(', ']' to '[', '}' to '{')
+        val key = arrayListOf<Char>()
+
         if (s.length <= 1) return false
         if (s.length % 2 != 0) return false
-        val map = mapOf<Char, Char>('(' to ')', '[' to ']', '{' to '}')
-        val key = arrayListOf<Char>()
-        if (!map.containsKey(s[0])) return false
-        s.forEachIndexed { index, c ->
-            if (map.containsKey(c)){
-                key.add(c)
-            } else if (map.containsValue(c)) {
-                if (key.size == 0 || map[key[key.lastIndex]] != c) return false
-               key.removeAt(key.size-1)
-            } else {
-                return false
+        if (!map.containsValue(s.first())) return false
+        if (!map.containsKey(s.last())) return false
+
+        for (c in s) {
+            when {
+                map.containsValue(c) -> key.add(c)
+                map.containsKey(c) -> if (key.isEmpty() || map[c] != key.removeAt(key.size-1)) return false
+                else -> return false
             }
-            if (index == s.length-1 && key.size == 0) return true
         }
-        return false
+
+        return key.isEmpty()
     }
 }
